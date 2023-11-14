d9c739e3d6d9fcb79fa42f638a400a1f82f6f595
Pouyan Samimi <pooyan.samimi@yahoo.com>
2023-11-14 14:23:01 +0330

    Refactored list comprehension to a generator expression for improved performance.

--- problem1/solution.py
+++ problem1/solution.py
@@ -1,5 +1,5 @@
 def solution(limit):
-    return sum([x for x in range(limit) if x%3 == 0 or x%5 == 0])
+    return sum(x for x in range(limit) if x%3 == 0 or x%5 == 0)
 
 limit = 1000
 print (solution(limit))
